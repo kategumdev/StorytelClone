93af311fd78a8f4285e7eaef512b484b96fcd207
Kateryna Gumenna <texturedandcozy@gmail.com>
2023-05-12 12:24:07 +0200

    Refactor button in PlaySampleButtonContainer to get label with only 1 line
    Create custom label and imageView, because button configured with
    UIButton.Configuration doesn't let to set number of lines for label and default button
    doesn't let to set padding between imageView and label.

--- StorytelClone/Views/PlaySampleButton.swift
+++ StorytelClone/Views/PlaySampleButton.swift
@@ -11,6 +11,9 @@ class PlaySampleButtonContainer: UIView {
     // MARK: - Static properties
     static let buttonHeight: CGFloat = RoundButtonsStackContainer.roundWidth
     
+    static func getLabelScaledFont() -> UIFont {
+        return UIFontMetrics.default.scaledFont(for: Utils.navBarTitleFont, maximumPointSize: 40)
+    }
     // MARK: - Instance properties
     private let button: UIButton = {
         let button = UIButton()
@@ -18,93 +21,42 @@ class PlaySampleButtonContainer: UIView {
         button.layer.borderWidth = 2
         button.layer.borderColor = UIColor.label.cgColor
         button.layer.cornerRadius = PlaySampleButtonContainer.buttonHeight / 2
-        
-        button.titleLabel?.lineBreakMode = .byTruncatingTail
-        button.titleLabel?.numberOfLines = 1
-        
-        let scaledFont = UIFontMetrics.default.scaledFont(for: Utils.navBarTitleFont, maximumPointSize: 40)
-        button.titleLabel?.font = scaledFont
-        
-        let title = NSAttributedString(string: "Play a beautiful sample")
-        button.setAttributedTitle(title, for: .normal)
-        
-        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 18, weight: .semibold)
-        let image = UIImage(systemName: "play.fill", withConfiguration: symbolConfig)
-        button.setImage(image, for: .normal)
-                
-        // Adjust the padding between the title and image
-//        let padding: CGFloat = 10.0
-//        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: padding, bottom: 0, right: -padding)
-//        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -padding, bottom: 0, right: padding)
-        
-        button.titleLabel?.translatesAutoresizingMaskIntoConstraints = false
-        guard let label = button.titleLabel, let imageView = button.imageView else { return button }
-        let constraint = label.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 12)
-        constraint.priority = .defaultHigh
-        constraint.isActive = true
-//        label.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 12).isActive = true
         return button
     }()
     
-//    private let button: UIButton = {
-//        let button = UIButton()
-//        button.tintColor = UIColor.label
-//        button.layer.borderWidth = 2
-//        button.layer.borderColor = UIColor.label.cgColor
-//        button.layer.cornerRadius = PlaySampleButtonContainer.buttonHeight / 2
-//
-////        button.titleLabel?.lineBreakMode = .byTruncatingTail
-////        button.titleLabel?.numberOfLines = 1
-//
-//        var buttonConfig = UIButton.Configuration.plain()
-//        buttonConfig.attributedTitle = AttributedString("Play a beautiful sample")
-//
-////        buttonConfig.attributedTitle?.paragraphStyle?.lineBreakMode = .byTruncatingMiddle
-//
-////        // Customize the title attributes
-//        let paragraphStyle = NSMutableParagraphStyle()
-//        paragraphStyle.lineBreakMode = .byTruncatingTail
-//        paragraphStyle.lineBreakStrategy
-////
-////
-//        let titleAttributes: [NSAttributedString.Key: Any] = [
-//            .paragraphStyle: paragraphStyle,
-//        ]
-//
-//        let attributedString = NSMutableAttributedString(
-//            string: "Your long text here",
-//            attributes: titleAttributes
-//        )
-//
-//        button.setAttributedTitle(attributedString, for: .normal)
-//
-////        buttonConfig.attributedTitle?.setAttributes(titleAttributes)
-//
-//
-////        button.configuration?.attributedTitle = NSAttributedString(
-////            string: "Your long text here",
-////            attributes: titleAttributes
-////        )
-//
-//        buttonConfig.attributedTitle?.font = Utils.navBarTitleFont
-//        buttonConfig.titleAlignment = .center
-////        buttonConfig.contentInsets = .zero
-//        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 18, weight: .semibold)
-//        let image = UIImage(systemName: "play.fill", withConfiguration: symbolConfig)
-//        buttonConfig.image = image
-//        buttonConfig.imagePlacement = .leading
-//        buttonConfig.imagePadding = 12
-//        button.configuration = buttonConfig
-//        // Limit the number of lines to 1
-//        button.titleLabel?.numberOfLines = 1
-//        return button
-//    }()
+    private let customImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 19, weight: .semibold)
+        let image = UIImage(systemName: "play.fill", withConfiguration: symbolConfig)
+        imageView.image = image
+        return imageView
+    }()
+    
+    private let customTitleLabel: UILabel = {
+        let label = UILabel()
+        label.lineBreakMode = .byTruncatingTail
+        label.text = "Play a sample"
+        label.textAlignment = .left
+        label.font = getLabelScaledFont()
+        return label
+    }()
+    
+    private lazy var horzStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 12
+        stack.addArrangedSubview(customImageView)
+        stack.addArrangedSubview(customTitleLabel)
+        return stack
+    }()
 
     // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = Utils.customBackgroundColor
         addSubview(button)
+        button.addSubview(horzStack)
         applyConstraints()
     }
     
@@ -121,9 +73,7 @@ class PlaySampleButtonContainer: UIView {
         }
         
         if previousTraitCollection?.preferredContentSizeCategory != traitCollection.preferredContentSizeCategory {
-            let scaledFont = UIFontMetrics.default.scaledFont(for: Utils.navBarTitleFont, maximumPointSize: 40)
-            button.titleLabel?.font = scaledFont
-//            button.configuration?.attributedTitle?.font = scaledFont
+            customTitleLabel.font = PlaySampleButtonContainer.getLabelScaledFont()
         }
     }
     
@@ -135,6 +85,14 @@ class PlaySampleButtonContainer: UIView {
             button.widthAnchor.constraint(equalTo: widthAnchor, constant: -Constants.commonHorzPadding * 2),
             button.topAnchor.constraint(equalTo: topAnchor),
             button.centerXAnchor.constraint(equalTo: centerXAnchor),
+        ])
+        
+        horzStack.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            horzStack.centerXAnchor.constraint(equalTo: button.centerXAnchor),
+            horzStack.centerYAnchor.constraint(equalTo: button.centerYAnchor),
+            horzStack.leadingAnchor.constraint(greaterThanOrEqualTo: button.leadingAnchor, constant: 10),
+            horzStack.trailingAnchor.constraint(lessThanOrEqualTo: button.trailingAnchor, constant: -10)
         ])
     }
     
