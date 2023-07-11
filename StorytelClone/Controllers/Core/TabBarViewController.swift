e3dcc91ba9cb2eb02c3b94027d1c5957abb18bb8
Kateryna Gumenna <texturedandcozy@gmail.com>
2023-07-11 22:19:12 +0200

    Refactor TabBarViewController

--- StorytelClone/Controllers/Core/TabBarViewController.swift
+++ StorytelClone/Controllers/Core/TabBarViewController.swift
@@ -11,13 +11,28 @@ class TabBarViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureSelf()
+    }
+    
+    // MARK: - Helper methods
+    private func configureSelf() {
         view.backgroundColor = UIColor.customBackgroundColor
+        configureRootVCs()
+        configureTabBar()
+    }
+
+    private func configureRootVCs() {
+        let homeVC = HomeViewController(categoryModel: Category.home)
+        let vc1 = UINavigationController(rootViewController: homeVC)
         
-        let vc1 = UINavigationController(rootViewController: HomeViewController(categoryModel: Category.home))
-        let vc2 = UINavigationController(rootViewController: SearchViewController(categoryModel: Category.searchVc, categoriesForButtons: Category.categoriesForAllCategories))
+        let searchVC = SearchViewController(
+            categoryModel: Category.searchVc,
+            categoriesForButtons: Category.categoriesForAllCategories)
+        let vc2 = UINavigationController(rootViewController: searchVC)
+        
         let vc3 = UINavigationController(rootViewController: BookshelfViewController())
         let vc4 = UINavigationController(rootViewController: ProfileViewController())
-        
+                
         let symbolConfig = UIImage.SymbolConfiguration(pointSize: 16, weight: .medium)
         
         vc1.tabBarItem.image = UIImage(systemName: "lightbulb")?.withConfiguration(symbolConfig)
@@ -30,31 +45,43 @@ class TabBarViewController: UITabBarController {
         vc3.title = "Bookshelf"
         vc4.title = "Profile"
         
+        setViewControllers([vc1, vc2, vc3, vc4], animated: false)
+    }
+    
+    private func configureTabBar() {
         tabBar.isTranslucent = false
-        
-        // This line avoids tab bar blinking gray when next vc is being pushed
-        tabBar.backgroundColor = UIColor.customBackgroundColor
-        
         tabBar.standardAppearance.backgroundEffect = nil
         tabBar.standardAppearance.shadowColor = .tertiaryLabel
-                
-        setTabBarItemColors([tabBar.standardAppearance.stackedLayoutAppearance, tabBar.standardAppearance.inlineLayoutAppearance, tabBar.standardAppearance.compactInlineLayoutAppearance])
-        
         tabBar.scrollEdgeAppearance = tabBar.standardAppearance
-        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        setTabBarItemColors()
+        
+        // This line avoids the tab bar blinking gray when next vc is being pushed
+        tabBar.backgroundColor = UIColor.customBackgroundColor
     }
-
     
-    private func setTabBarItemColors(_ itemAppearances: [UITabBarItemAppearance]) {
+    private func setTabBarItemColors() {
         let normalColor = UIColor.label
         let selectedColor = UIColor.customTintColor
         let font = UIFont.createStaticFontWith(weight: .semibold, size: 11)
         
+        let itemAppearances = [
+            tabBar.standardAppearance.stackedLayoutAppearance,
+            tabBar.standardAppearance.inlineLayoutAppearance,
+            tabBar.standardAppearance.compactInlineLayoutAppearance
+        ]
+        
         for appearance in itemAppearances {
             appearance.normal.iconColor = normalColor
-            appearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: normalColor, NSAttributedString.Key.font: font]
+            appearance.normal.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor: normalColor,
+                NSAttributedString.Key.font: font
+            ]
+            
             appearance.selected.iconColor = selectedColor
-            appearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: selectedColor, NSAttributedString.Key.font: font]
+            appearance.selected.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor: selectedColor,
+                NSAttributedString.Key.font: font
+            ]
         }
     }
 
