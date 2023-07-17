691874c4bf3cb70b026b1931dff30b3bffa843f7
Kateryna Gumenna <texturedandcozy@gmail.com>
2023-07-17 13:14:43 +0200

    Refactor OneBookOverviewCellSubviewsContainer

--- StorytelClone/Views/BookWithOverviewCellSubviewsContainer.swift
+++ StorytelClone/Views/BookWithOverviewCellSubviewsContainer.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+/// Created as a separate class to enable calculation of OneBookOverviewTableViewCell height
 class OneBookOverviewCellSubviewsContainer: UIView {
     // MARK: - Static properties
     static let borderColor = UIColor(named: "borderBookOverview")
@@ -15,7 +16,7 @@ class OneBookOverviewCellSubviewsContainer: UIView {
     // MARK: - Instance properties
     private var book: Book?
     private let dataPersistenceManager: any DataPersistenceManager
-        
+    
     let dimmedAnimationButton: DimmedAnimationButton = {
         let button = DimmedAnimationButton()
         button.backgroundColor = backgroundColor
@@ -24,17 +25,17 @@ class OneBookOverviewCellSubviewsContainer: UIView {
         return button
     }()
     
-    private let squareImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.layer.cornerRadius = Constants.commonBookCoverCornerRadius
-        imageView.clipsToBounds = true
-        imageView.contentMode = .scaleAspectFill
-        return imageView
+    private lazy var vertStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.isUserInteractionEnabled = false
+        stack.axis = .vertical
+        stack.alignment = .fill
+        stack.distribution = .equalCentering
+        [bookTitleLabel, overviewLabel, starHorzStackView].forEach { stack.addArrangedSubview($0) }
+        stack.setCustomSpacing(12.0, after: bookTitleLabel)
+        stack.setCustomSpacing(15, after: overviewLabel)
+        return stack
     }()
-    
-    private lazy var saveButton = SaveBookButton()
-    
-    var saveBookButtonDidTapCallback: SaveBookButtonDidTapCallback = {_ in}
     
     private let bookTitleLabel: UILabel = {
         let label = UILabel()
@@ -52,36 +53,30 @@ class OneBookOverviewCellSubviewsContainer: UIView {
         
     private lazy var starHorzStackView = StarHorzStackView(withSaveAndEllipsisButtons: false)
     
-    private lazy var vertStackView: UIStackView = {
-        let stack = UIStackView()
-        stack.isUserInteractionEnabled = false
-        stack.axis = .vertical
-        stack.alignment = .fill
-        stack.distribution = .equalCentering
-        [bookTitleLabel, overviewLabel, starHorzStackView].forEach { stack.addArrangedSubview($0) }
-        stack.setCustomSpacing(12.0, after: bookTitleLabel)
-        stack.setCustomSpacing(15, after: overviewLabel)
-        return stack
+    private let squareImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = Constants.commonBookCoverCornerRadius
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
     }()
+    
+    private lazy var saveButton = SaveBookButton()
+    
+    var saveBookButtonDidTapCallback: SaveBookButtonDidTapCallback = {_ in}
     
     // MARK: - Initializers
     init(dataPersistenceManager: some DataPersistenceManager = CoreDataManager.shared) {
         self.dataPersistenceManager = dataPersistenceManager
         super.init(frame: .zero)
-        addSubview(dimmedAnimationButton)
-        dimmedAnimationButton.addSubview(vertStackView)
-        addSubview(squareImageView)
-        addSubview(saveButton)
-        addSaveButtonAction()
-        dimmedAnimationButton.addConfigurationUpdateHandlerWith(viewToTransform: self)
-        applyConstraints()
+        configureSelf()
     }
     
     required init?(coder: NSCoder) {
         fatalError("BookCollectionViewCell is not configured to be instantiated from storyboard")
     }
     
-    // MARK: - View life cycle
+    // MARK: -
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         
@@ -102,19 +97,39 @@ class OneBookOverviewCellSubviewsContainer: UIView {
         }
         
         self.book = book
+        
         let titleString = book.title
         bookTitleLabel.attributedText = NSAttributedString(string: titleString).withLineHeightMultiple(0.8)
+        
         let overviewString = book.overview
         overviewLabel.attributedText = NSAttributedString(string: overviewString).withLineHeightMultiple(0.9)
+        
         starHorzStackView.isHidden = false
         starHorzStackView.configureWith(book: book)
+        
         squareImageView.image = book.coverImage
+        
         dimmedAnimationButton.kind = .toPushBookVcWith(book)
+        
         saveButton.isHidden = false
         updateSaveButton(isBookBeingAdded: book.isOnBookshelf())
       }
     
     // MARK: - Helper methods
+    private func configureSelf() {
+        setupUI()
+        addSaveButtonAction()
+        dimmedAnimationButton.addConfigurationUpdateHandlerWith(viewToTransform: self)
+    }
+    
+    private func setupUI() {
+        addSubview(dimmedAnimationButton)
+        dimmedAnimationButton.addSubview(vertStackView)
+        addSubview(squareImageView)
+        addSubview(saveButton)
+        applyConstraints()
+    }
+    
     private func addSaveButtonAction() {
         saveButton.addAction(UIAction(handler: { [weak self] _ in
             self?.handleSaveButtonTapped()
@@ -144,45 +159,72 @@ class OneBookOverviewCellSubviewsContainer: UIView {
     
     private func applyConstraints() {
         translatesAutoresizingMaskIntoConstraints = false
-        // These two constraints are used only when creating container for calculation of row height for the cell using this container
-        widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
-        heightAnchor.constraint(equalTo: dimmedAnimationButton.heightAnchor, constant: 15).isActive = true
+        /* These two constraints are useful only when creating container for calculation of row height
+         for the cell using this container */
+        NSLayoutConstraint.activate([
+            widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
+            heightAnchor.constraint(
+                equalTo: dimmedAnimationButton.heightAnchor,
+                constant: 15)
+        ])
         
         let leadingConstant: CGFloat = 24
         let imageTopConstant: CGFloat = 8
         let paddingBetweenImageAndStack: CGFloat = 17
         
         vertStackView.translatesAutoresizingMaskIntoConstraints = false
-        let topAnchorConstant: CGFloat = (Constants.mediumSquareBookCoverSize.height - imageTopConstant) + paddingBetweenImageAndStack
+        let topAnchorConstant: CGFloat =
+        Constants.mediumSquareBookCoverSize.height - imageTopConstant + paddingBetweenImageAndStack
         NSLayoutConstraint.activate([
-            vertStackView.topAnchor.constraint(equalTo: dimmedAnimationButton.topAnchor, constant: topAnchorConstant),
-            vertStackView.leadingAnchor.constraint(equalTo: dimmedAnimationButton.leadingAnchor, constant: leadingConstant),
-            vertStackView.widthAnchor.constraint(equalTo: dimmedAnimationButton.widthAnchor, constant: -(leadingConstant * 2))
+            vertStackView.topAnchor.constraint(
+                equalTo: dimmedAnimationButton.topAnchor,
+                constant: topAnchorConstant),
+            vertStackView.leadingAnchor.constraint(
+                equalTo: dimmedAnimationButton.leadingAnchor,
+                constant: leadingConstant),
+            vertStackView.widthAnchor.constraint(
+                equalTo: dimmedAnimationButton.widthAnchor,
+                constant: -(leadingConstant * 2))
         ])
  
-        let buttonTopConstant: CGFloat = 15 // Creates padding between section header label and this button
+        let buttonTopConstant: CGFloat = 15 // Padding between section header label and this button
         dimmedAnimationButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            dimmedAnimationButton.topAnchor.constraint(equalTo: topAnchor, constant: buttonTopConstant),
-            dimmedAnimationButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.commonHorzPadding),
-            dimmedAnimationButton.widthAnchor.constraint(equalTo: widthAnchor, constant: -(Constants.commonHorzPadding * 2)),
-            dimmedAnimationButton.bottomAnchor.constraint(equalTo: vertStackView.bottomAnchor, constant: 14)
+            dimmedAnimationButton.topAnchor.constraint(
+                equalTo: topAnchor,
+                constant: buttonTopConstant),
+            dimmedAnimationButton.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: Constants.commonHorzPadding),
+            dimmedAnimationButton.widthAnchor.constraint(
+                equalTo: widthAnchor,
+                constant: -(Constants.commonHorzPadding * 2)),
+            dimmedAnimationButton.bottomAnchor.constraint(
+                equalTo: vertStackView.bottomAnchor,
+                constant: 14)
         ])
         
         squareImageView.translatesAutoresizingMaskIntoConstraints = false
         let imageWidthAndHeight: CGFloat = Constants.mediumSquareBookCoverSize.width
         NSLayoutConstraint.activate([
-            squareImageView.topAnchor.constraint(equalTo: dimmedAnimationButton.topAnchor, constant: -imageTopConstant),
-            squareImageView.leadingAnchor.constraint(equalTo: dimmedAnimationButton.leadingAnchor, constant: leadingConstant),
+            squareImageView.topAnchor.constraint(
+                equalTo: dimmedAnimationButton.topAnchor,
+                constant: -imageTopConstant),
+            squareImageView.leadingAnchor.constraint(
+                equalTo: dimmedAnimationButton.leadingAnchor,
+                constant: leadingConstant),
             squareImageView.widthAnchor.constraint(equalToConstant: imageWidthAndHeight),
             squareImageView.heightAnchor.constraint(equalToConstant: imageWidthAndHeight)
         ])
         
         saveButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            saveButton.topAnchor.constraint(equalTo: dimmedAnimationButton.topAnchor, constant: 23),
-            saveButton.trailingAnchor.constraint(equalTo: dimmedAnimationButton.trailingAnchor, constant: -23)
+            saveButton.topAnchor.constraint(
+                equalTo: dimmedAnimationButton.topAnchor,
+                constant: 23),
+            saveButton.trailingAnchor.constraint(
+                equalTo: dimmedAnimationButton.trailingAnchor,
+                constant: -23)
         ])
     }
-
 }
